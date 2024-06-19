9d7be66b6fd5ed516db6dcfbb410e78b9ee52582
Gersin <gersonfernandesribeiro48@gmail.com>
2024-06-19 17:50:53 -0300

    commit

--- PackArquivos/__init__.py
+++ PackArquivos/__init__.py
@@ -1,4 +1,5 @@
 import secrets
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -10,8 +11,13 @@ login_manager = LoginManager(app)
 
 tokenForms = secrets.token_hex(16)
 app.config['SECRET_KEY'] = tokenForms
-app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql://root:JCKcEWuUlQXQPHncWAPMBObeEuCTyVna@viaduct.proxy.rlwy.net:34525'
-                                         '/railway')
+
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recibos.db'
+
+# ('mysql://root:JCKcEWuUlQXQPHncWAPMBObeEuCTyVna@viaduct.proxy.rlwy.net:34525/railway')
 
 db = SQLAlchemy(app)
 login_manager.login_view = 'login'
